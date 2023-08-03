0066637c2c835e91d7de3285f167769b64b9c661
Rafael Zimmer <rzimmerdev@gmail.com>
2023-08-03 01:49:29 -0300

    Added playlists, cache and lyrics api

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# playlist-manager
+# song-manager
 Tired of manually managing your songs, lyrics and albums? Yeah, me too
--- youtube_download.py
+++ youtube_download.py
@@ -1,66 +1,134 @@
 import os
 import sys
-from pytube import YouTube
+import re
+
+import lyricsgenius
+from pytube import YouTube, Playlist
 import requests
 from moviepy.editor import AudioFileClip
 import eyed3
+from getpass import getpass
+
+from pytube.exceptions import RegexMatchError
 
 
-def download_video(video_url, folder):
+def update_metadata(path, title=None, author=None, thumbnail_url=None, lyrics=None):
+    audio = eyed3.load(path)
+    if audio.tag is None:
+        audio.initTag()
+
+    thumbnail_url = thumbnail_url
+    thumbnail_response = requests.get(thumbnail_url)
+    thumbnail_data = thumbnail_response.content
+
+    audio.tag.title = title
+    if author:
+        audio.tag.artist = author
+    if thumbnail_data:
+        audio.tag.images.set(3, thumbnail_data, 'image/jpeg', u"Front cover")
+    if lyrics:
+        audio.tag.lyrics.set(lyrics)
+
+    audio.tag.save()
+
+
+def convert_to_mp3(title, path, folder="songs"):
+    new_path = folder + "/" + title + '.mp3'
+
+    # Convert the audio to MP3 format using moviepy
+    audio_clip = AudioFileClip(path)
+    audio_clip.write_audiofile(new_path, codec='mp3', verbose=False)
+    audio_clip.close()
+
+    os.remove(path)  # Remove the original file
+    return new_path
+
+
+def get_video(video_url, folder_path):
+    yt = YouTube(video_url)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    formatted_title = re.sub('[^A-Za-z0-9 ]+', '', yt.title).replace(" ", "_")
+
+    return yt, formatted_title
+
+
+def download_video(video_url, folder="songs", overwrite=False, genius_object=None):
+    folder_path = os.path.join(os.getcwd(), folder)
+
     try:
-        yt = YouTube(video_url)
+        yt, formatted_title = get_video(video_url, folder_path)
 
+        if formatted_title + ".mp3" in os.listdir(folder_path):
+            if overwrite:
+                os.remove(os.path.join(folder_path, formatted_title + ".mp3"))
+            else:
+                print("Already downloaded ", yt.title, "\n")
+                return
+
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        raw_file = audio_stream.download(output_path=folder_path, filename=formatted_title)
+        mp3_file = convert_to_mp3(formatted_title, raw_file, folder)
+
+        lyrics = None
+        if genius_object and yt.title:
+            if yt.author:
+                song = genius_object.search_song(yt.title, yt.author)
+            else:
+                song = genius_object.search_song(yt.title)
+
+            lyrics = song.lyrics
+
+        update_metadata(mp3_file, yt.title, yt.author, yt.thumbnail_url, lyrics)
+
+        print("Finished saving ", yt.title, "\n")
+
+    except (ConnectionError, OSError) as e:
+        print(f"Error - couldn't download: {video_url}", e)
+    except KeyboardInterrupt:
+        yt, formatted_title = get_video(video_url, folder_path)
+        print("\nPossibly corrupted file: ", os.path.join(formatted_title + ".mp3"))
+        raise KeyboardInterrupt
+
+
+def download_playlist(playlist_url, folder="songs", overwrite=False, genius_object=None):
+    try:
+        playlist = Playlist(playlist_url)
         folder_path = os.path.join(os.getcwd(), folder)
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        print("Downloading:", yt.title)
-
-        audio_stream = yt.streams.filter(only_audio=True).first()
-
-        out = audio_stream.download(output_path=folder_path, filename=yt.title)
-
-        base, ext = os.path.splitext(out)
-        audio_file = base + '.mp3'
-
-        # Convert the audio to MP3 format using moviepy
-        audio_clip = AudioFileClip(out)
-        audio_clip.write_audiofile(audio_file, codec='mp3', verbose=False)
-        audio_clip.close()
-
-        os.remove(out)  # Remove the original file
-
-        print("Downloaded:", yt.title)
-        print("Saving metadata:", yt.title)
-
-        audio = eyed3.load(audio_file)
-        if audio.tag is None:
-            audio.initTag()
-
-        audio.tag.title = yt.title
-        audio.tag.artist = yt.author
-
-        thumbnail_url = yt.thumbnail_url
-        thumbnail_response = requests.get(thumbnail_url)
-        thumbnail_data = thumbnail_response.content
-
-        audio.tag.images.set(3, thumbnail_data, 'image/jpeg', u"Front cover")
-
-        audio.tag.save()
-
-        print("Metadata saved:", yt.title)
+        for video_url in playlist.video_urls:
+            download_video(video_url, folder, overwrite, genius_object)
 
     except ConnectionError as e:
         print("Error:", e)
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python youtube_download.py <video_url>")
-        return
+    try:
+        if len(sys.argv) < 2 or len(sys.argv) > 4 or "-h" in sys.argv:
+            print("Usage: youtube_download.py [-g] [-u] <video_url>\n")
+            return
 
-    video_url = sys.argv[1]
-    download_video(video_url, "songs")
+        genius = None
+        overwrite = False
+        if "-g" in sys.argv:
+            token = getpass("Genius API access token: \n")
+            genius = lyricsgenius.Genius(token)
+        if "-u" in sys.argv:
+            overwrite = True
+
+        url = sys.argv[-1]
+        if "youtube.com/playlist" in url:
+            download_playlist(url, "songs", overwrite, genius)
+        else:
+            download_video(url, "songs", overwrite, genius)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+    except RegexMatchError:
+        print("Usage: youtube_download.py [-g] [-u] <video_url>\n")
 
 
 if __name__ == "__main__":
